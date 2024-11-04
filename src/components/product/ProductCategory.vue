4a906dfd31d8e01b72dc32bcd5cd3fe32ad116e4
Lyndsey Nguyen <lyndseynguyen@infra04-wg324.eduroam.wireless.umn.edu>
2024-11-04 16:54:41 -0600

    started final layout

--- src/components/product/ProductCategory.vue
+++ src/components/product/ProductCategory.vue
@@ -1,18 +1,39 @@
 <script setup>
 
 import PrimaryTemplate from "../../Template/PrimaryTemplate.vue";
+import productData from '../../MOCK_DATA.js'
 </script>
 
 <template>
   <primary-template>
     <h1>This is where I'll show all the products I sell!</h1>
 
+    <div class="products">
+
+      <div class="product" v-for="(product, index) in productData.data" :key="index">
+        {{ product.first_name }} {{ product.last_name }}
+      </div>
+
+    </div>
+
   </primary-template>
 </template>
-  <div class="logo">
+<div class="logo">
 
 
-  </div>
+</div>
+
 <style scoped>
+.products {
+  font-size: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
 
+  .product {
+    border: 1px solid #ddd;
+    font-weight: bold;
+    min-width: 300px;
+  }
+}
 </style>
