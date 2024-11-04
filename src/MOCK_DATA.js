6da86b8a8fc9f5c307a1b89725f3691c05e70eb6
Lyndsey Nguyen <lyndseynguyen@infra04-wg324.eduroam.wireless.umn.edu>
2024-11-04 16:54:55 -0600

    started final layout 2

---
+++ src/MOCK_DATA.js
@@ -0,0 +1,84 @@
+const data = [
+  {
+    "id": null,
+    "first_name": "Vale",
+    "last_name": "Stonehouse",
+    "email": "vstonehouse0@columbia.edu",
+    "gender": "Male",
+    "ip_address": "90.211.20.25"
+  },
+  {
+    "id": null,
+    "first_name": "Chester",
+    "last_name": "Blackster",
+    "email": "cblackster1@ibm.com",
+    "gender": "Male",
+    "ip_address": "236.195.62.187"
+  },
+  {
+    "id": null,
+    "first_name": "Jedd",
+    "last_name": "Realff",
+    "email": "jrealff2@google.cn",
+    "gender": "Male",
+    "ip_address": "73.112.102.77"
+  },
+  {
+    "id": null,
+    "first_name": "Franny",
+    "last_name": "Gergus",
+    "email": "fgergus3@live.com",
+    "gender": "Female",
+    "ip_address": "177.160.222.228"
+  },
+  {
+    "id": null,
+    "first_name": "Ealasaid",
+    "last_name": "Gallier",
+    "email": "egallier4@constantcontact.com",
+    "gender": "Female",
+    "ip_address": "69.147.173.215"
+  },
+  {
+    "id": null,
+    "first_name": "Antonetta",
+    "last_name": "Mulran",
+    "email": "amulran5@squidoo.com",
+    "gender": "Female",
+    "ip_address": "83.72.66.5"
+  },
+  {
+    "id": null,
+    "first_name": "Lowe",
+    "last_name": "Adanet",
+    "email": "ladanet6@merriam-webster.com",
+    "gender": "Male",
+    "ip_address": "59.190.145.254"
+  },
+  {
+    "id": null,
+    "first_name": "Mar",
+    "last_name": "Gilks",
+    "email": "mgilks7@techcrunch.com",
+    "gender": "Male",
+    "ip_address": "27.27.214.89"
+  },
+  {
+    "id": null,
+    "first_name": "Barbette",
+    "last_name": "McGing",
+    "email": "bmcging8@topsy.com",
+    "gender": "Female",
+    "ip_address": "201.83.249.229"
+  },
+  {
+    "id": null,
+    "first_name": "Clovis",
+    "last_name": "Dalziell",
+    "email": "cdalziell9@ehow.com",
+    "gender": "Female",
+    "ip_address": "148.232.50.2"
+  }
+]
+
+export default { data }
